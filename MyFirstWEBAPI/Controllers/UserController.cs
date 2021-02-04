f5b78bb4e99c8efb4467924f39f48efcce926a4f
Helena Copppieters <hcoppieters@hotmail.com>
2021-02-04 13:54:04 +0100

    UserController: Get lijst van users en Post nieuwe user

---
+++ MyFirstWEBAPI/Controllers/UserController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MyFirstWEBAPI.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyFirstWEBAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        private ToDoDbContext _context;
+        public UserController(ToDoDbContext context)
+        {
+            _context = context;  
+        }
+        //GET api/User (de lijst van alles users teruggeven)
+        [HttpGet]
+        public ActionResult<IEnumerable<User>> GetUser()
+        {
+            return Ok(_context.Users.ToList()); //OK = 200 status code 
+        }
+        //POST api/user  (een nieuwe User aanmaken)
+        [HttpPost]
+        public ActionResult<User> PostUser(User user)
+        {
+            _context.Users.Add(user);
+            _context.SaveChanges();
+
+            return CreatedAtAction("PostUser", user);
+        }
+    }
+}
