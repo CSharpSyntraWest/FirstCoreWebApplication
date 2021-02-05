24dd32a488fcfede79b5d9bb7c48527042017ab2
Helena Copppieters <hcoppieters@hotmail.com>
2021-02-05 11:29:16 +0100

    BrouwersController met Index action methode en Razor View

---
+++ MyFirstMVCWebApp/Controllers/BrouwersController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MyFirstMVCWebApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyFirstMVCWebApp.Controllers
+{
+    public class BrouwersController : Controller
+    {
+        private readonly BierenDbContext _context;
+
+        public BrouwersController(BierenDbContext context)
+        {
+            _context = context;
+        }
+        public IActionResult Index()
+        {
+            List<Brouwers> brouwers = _context.Brouwers.ToList();
+            return View(brouwers);
+        }
+    }
+}
